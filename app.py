7d9affe3f8cdf75355a2a8b53f1f3b56cad0b38f
KhawPeiQi <khawpeiqi@gmail.com>
2025-09-01 01:59:20 +0800

    update progress

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
 from flask import Flask, request, redirect, url_for, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+# add database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
+# secret key
+app.config['SECRET_KEY'] = "060226*"
+# initialize the database
+db = SQLAlchemy(app)
+
+# create a activity post model
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    author = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+# create a form class
+class NameForm(FlaskForm):
+    name = StringField("What's your name",validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
 
 activity_posts = []
 next_id = 0
@@ -32,7 +60,6 @@ def create():
             "location": location
         })
         next_id += 1
-
         return redirect(url_for("home"))
     return render_template("create.html")
 
