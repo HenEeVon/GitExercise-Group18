6262c673e78fb2dca0b05921eefb209b196e241e
KhawPeiQi <khawpeiqi@gmail.com>
2025-09-01 22:42:55 +0800

    fixed somethings about database and some features

--- app.py
+++ app.py
@@ -23,13 +23,8 @@ class Posts(db.Model):
     content = db.Column(db.Text)
     author = db.Column(db.String(255))
     location = db.Column(db.String(255))
-    event_date = db.Column(db.DateTime)
+    event_datetime = db.Column(db.String(255))
     date_posted = db.Column(db.DateTime, default=lambda: datetime.utcnow().replace(tzinfo=pytz.utc))
-
-    def local_event_date(self):
-        if self.event_date is None:
-            return None
-        return utc_dt.astimezone(MALAYSIA_TZ)
 
     def local_date_posted(self):
         if self.date_posted is None:
@@ -39,22 +34,17 @@ class Posts(db.Model):
 # Flask-WTF form
 class ActivityForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
-    content = StringField("Content", validators=[DataRequired()])
+    content = TextAreaField("Content", validators=[DataRequired()])
     location = StringField("Location", validators=[DataRequired()])
+    event_datetime = StringField("Event Date & Time (e.g. 2025-09-01, 8am - 10am)", validators=[DataRequired()])
     author = StringField("Author")
     submit = SubmitField("Post")
+
 
 @app.route("/")
 def home():
     posts = Posts.query.order_by(Posts.date_posted.desc()).all()
-    posts = Posts.query.order_by(Posts.date_posted.desc()).all()
     for post in posts:
-        # event_date handling
-        if post.event_date:
-            post.local_event_date = post.event_date.replace(tzinfo=pytz.utc).astimezone(MALAYSIA_TZ)
-        else:
-            post.local_event_date = None
-
         # posted date handling
         if post.date_posted:
             post.local_date_posted = post.date_posted.replace(tzinfo=pytz.utc).astimezone(MALAYSIA_TZ)
@@ -62,9 +52,11 @@ def home():
             post.local_date_posted = None
     return render_template("index.html", posts=posts)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404 
+
 
 # create post form
 @app.route("/create", methods=["GET", "POST"])
@@ -75,14 +67,15 @@ def create():
             title=form.title.data,
             content=form.content.data,
             location=form.location.data,
-            author=form.author.data if form.author.data else "Anonymous",
-            event_date=datetime.utcnow().replace(tzinfo=pytz.utc)
+            event_datetime=form.event_datetime.data,
+            author=form.author.data if form.author.data else "Anonymous"
         )
         db.session.add(new_post)
         db.session.commit()
         flash("Activity post created successfully!")
         return redirect(url_for("home"))
     return render_template("create.html", form=form)
+
 
 # delete post
 @app.route("/delete/<int:post_id>", methods=["POST"])
@@ -96,11 +89,6 @@ def delete(post_id):
 @app.route("/post/<int:post_id>")
 def post_detail(post_id):
     post = Posts.query.get_or_404(post_id)
-
-    if post.event_date:
-        post.local_event_date = post.event_date.replace(tzinfo=pytz.utc).astimezone(MALAYSIA_TZ)
-    else:
-        post.local_event_date = None
 
     if post.date_posted:
         post.local_date_posted = post.date_posted.replace(tzinfo=pytz.utc).astimezone(MALAYSIA_TZ)
