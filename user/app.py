5abbfb0f609b46b31c0a316be4fa130807bd71a5
LEE EE WEN <eewen0298@gmail.com>
2025-09-02 20:50:10 +0800

    fix: make login and register  email case-insensitive

--- user/app.py
+++ user/app.py
@@ -43,7 +43,7 @@ def home():
 @app.route("/register", methods=['POST','GET'])
 def register():
     if request.method =='POST':
-        user_email = request.form['user_email']
+        user_email = request.form['user_email'].strip().lower() 
         name = request.form['name']
         gender = request.form['gender']
         password = request.form['password']
@@ -56,15 +56,40 @@ def register():
 def login():
     message = ''
     if request.method == 'POST':
-        email = request.form['user_email']
+        email = request.form['user_email'].strip().lower() 
         password = request.form['password']
         users = get_users()
         for user in users:
             if user[0] == email and user[3] == password:  #user[0]is index of email
                 return f"Login successful! Welcome {user[1]}"
-        message = "Invalid email or password"
+            
+            message = "User not Found. Invalid email or password !!!" #loop finishes without returning
 
     return render_template('login.html', message=message)
+
+@app.route("/resetpass", methods=['GET','POST'])
+def resetpass():
+    if request.method == 'POST':
+        user_email = request.form.get('email')       # match the input name in HTML
+        new_password = request.form.get('new_password')
+
+        if not user_email or not new_password:
+            return 'Email and new password are required!', 400 # 400 for bad request,missing or invalid input
+
+        with sqlite3.connect(DB_path) as conn:
+            c = conn.cursor()
+            c.execute('SELECT * FROM users WHERE user_email = ?', (user_email,))
+            user = c.fetchone()
+            if user:
+                # Update password
+                c.execute('UPDATE users SET password = ? WHERE user_email = ?', (new_password, user_email))
+                conn.commit()
+                return 'Password updated successfully!'
+            else:
+                return 'Email not found!'
+
+    return render_template('resetpass.html')
+
 
 if __name__ =='__main__':
     init_db()
